78c88b1fbcbe33e3b468d49837906931409f3bdb
svyatoslavna <svyatoslavna.m@gmail.com>
2025-10-30 17:49:44 +0300

    Add files via upload

---
+++ 29_09_2025/hello.py
@@ -0,0 +1,6 @@
+def greet(greet_text):
+    return greet_text
+
+
+greeting = "Hello, world!"
+print(greet(greeting))
---
+++ 29_09_2025/playground.py
@@ -0,0 +1,18 @@
+greet_text = "Hello, World!"
+print(greet_text)
+
+# 1. Замените текст в переменной greet_text
+greet_text = "Hello, friend!"
+print(greet_text)
+
+# 2. Создайте новую переменную с любым текстом
+new_text = "Goodbye, friend!"
+print(new_text)
+
+# 3. Запишите new_text в greet_text
+greet_text = new_text
+print(greet_text)
+
+# 4. А теперь мы изменим свойства new_text
+new_text += " Wow!"
+print(new_text)
